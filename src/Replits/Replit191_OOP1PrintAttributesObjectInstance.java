4ac221194ec4bb2cc68464e88eb534f19532bcb3
SerhatAlturk <alturkserht@gmail.com>
2020-05-06 01:41:07 -0500

    Java Forever

---
+++ src/Replits/Replit191_OOP1PrintAttributesObjectInstance.java
@@ -0,0 +1,30 @@
+package Replits;
+
+//Created at: 1:29 AM
+// May, 06, 2020
+public class Replit191_OOP1PrintAttributesObjectInstance
+	{
+	public static void main(String[] args)
+	{
+
+		Atts a = new Atts();
+		a.name = "ball";
+		a.color = "red";
+		a.amount = 1;
+
+
+		System.out.println(a.asString());
+	}
+
+	static class Atts
+		{
+			String name;
+			String color;
+			int amount;
+
+			public String asString()
+			{
+				return "name: " + name + " color: " + color + " amount: " + amount;
+			}
+		}
+	}
