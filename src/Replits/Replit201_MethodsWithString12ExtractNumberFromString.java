ea5412d65829ea53363cabaf7b7b5f4acc9f4318
SerhatAlturk <alturkserht@gmail.com>
2020-05-06 01:21:50 -0500

    Java Forever

--- src/Replits/Replit201_MethodsWithString12ExtractNumberFromString.java
+++ src/Replits/Replit201_MethodsWithString12ExtractNumberFromString.java
@@ -1,18 +1,26 @@
 package Replits;
 
 public class Replit201_MethodsWithString12ExtractNumberFromString                                                                  //Date: 05 May 2020 -- Time: 7:44 PM
+{
+public static String extractNum(String s)
+{
+	char[] arr = s.toCharArray();
+	String CleanStr = "";
+
+	for (char each : arr)
 	{
-	public static String extractNum(String s)
-	{
-		char[] arr = s.toCharArray();
-		String CleanStr = "";
-		for (char each : arr)
+		if (Character.isDigit(each))
 		{
-			if (Character.isDigit(each))
-			{
-				CleanStr += each;
-			}
+			CleanStr += each;
 		}
-		return CleanStr;
 	}
-	}
+	return CleanStr;
+}
+
+public static void main(String[] args)
+{
+	String a = "123fiveguys123";
+
+	System.out.println(extractNum(a));
+}
+}
