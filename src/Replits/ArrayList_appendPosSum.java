bda86d2d00a8fee37e4906f30d7542f659b1520f
SerhatAlturk <alturkserht@gmail.com>
2020-05-11 20:36:57 -0500

    Default

--- src/Replits/ArrayList_appendPosSum.java
+++ src/Replits/ArrayList_appendPosSum.java
@@ -20,7 +20,29 @@ public class ArrayList_appendPosSum   //Created at: 8:17 PM - May, 11, 2020
 {
 	public static void main(String[] args)
 	{
-
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(3);
+		list.add(-2);
+		ArrayList<Integer> go = appendPosSum(list);
+		System.out.println(go);
 	}
 	//create your method below
+	public static ArrayList<Integer> appendPosSum(ArrayList<Integer> aList)
+	{
+		ArrayList<Integer> pos = new ArrayList<Integer>();
+		int temp;
+		int count = 0;
+		for(int i = 0; i<aList.size();i++)
+		{
+			temp = aList.get(i);
+			if(temp>0)
+			{
+				pos.add(temp);
+				count+=temp;
+			}
+		}
+		pos.add(count);
+		return pos;
+	}
 }
